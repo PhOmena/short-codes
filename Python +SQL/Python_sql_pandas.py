c4380fadf6e2d494b067690d2dc27c0b08524a44
Phelipe <Phomena_2016@outlook.com>
2022-05-09 12:11:44 -0300

    changing the files

--- Python +SQL/Python_sql_pandas.py
+++ Python +SQL/Python_sql_pandas.py
@@ -3,24 +3,24 @@ import pyodbc
 import time
 
 try:
-    # Passa os dados da conexão
+    # passa os dados da conexão
         # Driver = Se for SQL, SQL Server, caso seja outro, pesquisar
         # Server = Numero do server ou se tiver local, nome do dispositivo - Exemplo: "Desktop-2342H"
         # Database = Nome da base de dados que você quer consultar ou implementar
-    dados_conexao = ("Driver={SQL Server};""Server=10.100.48.9;""Database=BaseServico;")
+    dataConnection = ("Driver={SQL Server};""Server=10.100.48.9;""Database=BaseServico;")
 
-    # Fazendo a conexão com o servidor
-    conexao = pyodbc.connect(dados_conexao)
+    # fazendo a conexão com o servidor
+    connection = pyodbc.connect(dataConnection)
     print("Conexão com o servidor estabelecida com sucesso!")
     time.sleep(3)
 
-    # Utilizando o PANDAS para ler o SQL
-    totalDataBase = pd.read_sql('SELECT * FROM BaseServico.dbo.*nome_da_tabela*', conexao)
+    # utilizando o PANDAS para ler o SQL
+    totalBase = pd.read_sql('SELECT * FROM BaseServico.dbo.*nome_da_tabela*', connection)
     print("Informação encontrada")
     time.sleep(5)
 
-    # Importando para um CSV
-    totalDataBase.to_csv("./arquivo.csv", index=False)
+    # importando para um CSV
+    totalBase.to_csv("./arquivo.csv", index=False)
 except:
     print("Não foi possível")
 
